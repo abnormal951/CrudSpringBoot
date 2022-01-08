eee845fb299e022587ac0e767ed0020e36f21779
Nabor <santosna@globalhitss.com>
2022-01-07 23:18:31 -0600

    se crean los metodos restantes del crud, POST, PUT, DELETE

--- src/main/java/com/sv/controlador/UserControlador.java
+++ src/main/java/com/sv/controlador/UserControlador.java
@@ -3,7 +3,12 @@ package com.sv.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +30,21 @@ public class UserControlador {
 	}
 	
 	
+	@PostMapping
+	public void insertar (@RequestBody User us)
 	
+	{
+		interfaceUser.save(us);
+	}
+	@PutMapping
+	public void modificar (@RequestBody User us)
+	
+	{
+		interfaceUser.save(us);
+	}
+	
+	@DeleteMapping("/{id}") 
+	public void eliminar(@PathVariable("id") Integer id) {
+		interfaceUser.deleteById(id);
+	}
 }
